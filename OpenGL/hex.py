70058ac6349153a89a5fa82c28b89b1aef40acb8
Eduardo Moura <79063539+EduMoura-debug@users.noreply.github.com>
2022-06-01 20:47:20 -0300

    Add files via upload

---
+++ OpenGL/hex.py
@@ -0,0 +1,84 @@
+import sdl2
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+
+cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
+
+a = 0
+def desenhaPiramide():
+    # glPushMatrix()
+    r = 5
+    ang = math.pi/3
+    glColor3fv(cores[0])
+    glBegin(GL_POLYGON)
+    i = 0
+    pontos_base = []
+    for i in range(0,6):
+        
+        x = r * math.cos(i*ang)
+        z = r * math.sin(i*ang)
+        pontos_base.append((x,0,z))
+        glVertex3f(x,0,z)
+        i = i+1
+
+    glEnd()
+    # glPopMatrix()
+    
+    glBegin(GL_TRIANGLE_FAN)
+    j = 0
+    glVertex3f(0,5,0)
+    for j in range(0,6):    
+        glColor3fv(cores[j+2])
+        
+        glVertex3fv(pontos_base[j])     
+        j += 1
+    
+    glEnd()
+    
+
+
+def desenha():
+    global a
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+    glTranslatef(0,-2,0)
+    # glRotatef(-a,0,0,1)
+    glRotatef(a,1,0,0)
+    desenhaPiramide()
+    glPopMatrix()
+    a += 1
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_MAJOR_VERSION, 2)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_MINOR_VERSION, 1)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_PROFILE_MASK,sdl2.SDL_GL_CONTEXT_PROFILE_CORE)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_DOUBLEBUFFER, 1)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_DEPTH_SIZE, 24)
+sdl2.SDL_GL_SetSwapInterval(1)
+window = sdl2.SDL_CreateWindow(b"Cubo", sdl2.SDL_WINDOWPOS_CENTERED, sdl2.SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, sdl2.SDL_WINDOW_OPENGL | sdl2.SDL_WINDOW_SHOWN)
+if not window:
+    sys.stderr.write("Error: Could not create window\n")
+    exit(1)
+glcontext = sdl2.SDL_GL_CreateContext(window)
+glEnable(GL_MULTISAMPLE)
+glEnable(GL_DEPTH_TEST)
+glClearColor(0.,0.,0.,1.)
+gluPerspective(45,800.0/600.0,0.1,100.0)
+glTranslatef(0.0,0.0,-20)
+
+running = True
+event = sdl2.SDL_Event()
+while running:
+    while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
+        if event.type == sdl2.SDL_QUIT:
+            running = False
+        if event.type == sdl2.events.SDL_KEYDOWN:
+            print("SDL_KEYDOWN")
+            if event.key.keysym.sym == sdl2.SDLK_ESCAPE:
+                running = False
+    desenha()
+    sdl2.SDL_GL_SwapWindow(window)
